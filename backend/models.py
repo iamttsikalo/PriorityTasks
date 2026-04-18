47ff009eea29bce929a23200160a846959529dbd
Цікало Максим <iamtsikalo@MacBook-Air-Cikalo.local>
2026-04-18 22:56:20 +0300

    fix: import User model and fix gunicorn crash

--- backend/models.py
+++ backend/models.py
@@ -2,14 +2,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
-    password_hash = db.Column(db.Text, nullable=False)
-    tasks = db.relationship('Task', backref='author', lazy=True)
-
 class Task(db.Model):
     __tablename__ = 'tasks'
 
@@ -26,3 +18,14 @@ class Task(db.Model):
             'importance': self.importance,
             'priority': self.urgency * self.importance
         }
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password_hash = db.Column(db.Text, nullable=False)
+    tasks = db.relationship('Task', backref='author', lazy=True)
+
+
+    
